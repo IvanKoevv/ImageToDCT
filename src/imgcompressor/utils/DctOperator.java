5577849a0aa3f744467a5447c55f3a2f0495b066
IvanKoevv <coldbeerhq@gmail.com>
2022-04-14 06:02:48 +0300

    removed unneeded lines

--- src/imgcompressor/utils/DctOperator.java
+++ src/imgcompressor/utils/DctOperator.java
@@ -9,7 +9,6 @@ public class DctOperator {
         doDctPerRow(src);
         src = transposeMatrix(src);
         doDctPerRow(src);
-        //src = transposeMatrix(src);
         return src;
     }
 
@@ -17,7 +16,6 @@ public class DctOperator {
         IDctPerRow(src);
         src = transposeMatrix(src);
         IDctPerRow(src);
-        //src = transposeMatrix(src);
         return src;
     }
     
